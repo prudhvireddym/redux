4645b75ac658d81c6f2f8b1169c378c5b61300be
Prudhvi Reddy <prudhvireddy@Prudhvis-MacBook-Air.local>
2022-01-26 17:26:58 +0530

    Added store and Provider to index.js

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider } from 'react-redux'
+
+
+const store = configureStore({
+  reducer:{}
+})
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
