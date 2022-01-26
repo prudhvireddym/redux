a100840754b9f4549e5054595ff7ed321b41644f
Prudhvi Reddy <prudhvireddy@Prudhvis-MacBook-Air.local>
2022-01-26 17:00:15 +0530

    initial commit

---
+++ redux-tutorial/src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div>
+        <h1>Profile Page</h1>
+        <p>Name :</p>
+        <p>age :</p>
+        <p>Email :</p>
+        </div>
+    )
+}
+
+export default Profile
