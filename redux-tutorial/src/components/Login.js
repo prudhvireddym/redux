7e32477201f96a3322f707cd447dfed0924f3616
Prudhvi Reddy <prudhvireddy@Prudhvis-MacBook-Air.local>
2022-01-26 20:53:42 +0530

    using useDispatch in Login.js to dispatch a payload to change the profile

--- redux-tutorial/src/components/Login.js
+++ redux-tutorial/src/components/Login.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {useDispatch} from 'react-redux'
+import {login} from '../features/user'
 
 function Login() {
+    const dispatch = useDispatch()
     return (
         <div>
-            <button>Login</button>
+            <button onClick={()=>{dispatch(login({name:"Prudhvi",age:0,email:"Prudhvi@gmail.com"}))}}>Login</button>
         </div>
     )
 }
